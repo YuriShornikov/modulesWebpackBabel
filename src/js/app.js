3e0c0f1f90dbf9c6c4c2343f9b7d2ead00f386c0
Yuri Shornikov <dbonlyfun@gmail.com>
2024-01-24 17:39:09 +0300

    webpack 1 and 2 task

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import { Game, GameSavingData, loadGame, saveGame } from './game';
+
+const game = new Game();
+game.start();
+
+
+// вывод первого задания
+console.log('app worked');
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader',],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    open: true,
+  },
+};
